7bdbbe66fbddc1e71eb0b27ad3889b1f00441300
Fahmida12345 <fahmidayesmin.me@gmail.com>
2023-12-06 21:08:37 +0600

    update

--- src/componants/Home.jsx
+++ src/componants/Home.jsx
@@ -6,8 +6,6 @@ import Hotsell from "./Hotsell";
 import Men from "./Men";
 import Ladies from "./Ladies";
 import Feedback from "./Feedback";
-import Footer from "./Footer";
-import Copyright from "./Copyright";
 
 const Home = () => {
   return (
